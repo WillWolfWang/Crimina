79179a2883c3f3dea433ef3aea2a321239674ed9
wangzhenhua <WillWolf_Wang@126.com>
2024-10-15 20:05:16 +0800

    练习 fragment 切换动画、Activity 显示 menu 菜单，fragment 隐藏 menu 菜单

--- app/src/main/java/com/will/criminalintent/TestActivity.kt
+++ app/src/main/java/com/will/criminalintent/TestActivity.kt
@@ -1,10 +1,13 @@
 package com.will.criminalintent
 
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -12,6 +15,10 @@ import com.google.android.material.navigation.NavigationBarView.OnItemSelectedLi
 
 class TestActivity: AppCompatActivity() {
     private lateinit var navView : BottomNavigationView
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
@@ -19,7 +26,31 @@ class TestActivity: AppCompatActivity() {
         navView = findViewById(R.id.nav_view)
         // 将 底部导航栏 和 中间的 fragment 关联起来，就可以自由切换了
         val fragmentContainerView =  supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
-        val navController = fragmentContainerView.navController
+        navController = fragmentContainerView.navController
         navView.setupWithNavController(navController)
+
+        // 将 actionBar 和 中间的 fragment 关联起来
+        appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
+
+    // 给 Activity 创建 menu 菜单
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_about, menu)
+        return true
+    }
+
+    // 可以使用 NavigationUI 自动导航，因为 menu 的 itemId 和 fragment 的 id 一样
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return  NavigationUI.onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item)
+    }
+
+    // actionBar 中 左侧返回按钮的事件处理
+    override fun onSupportNavigateUp(): Boolean {
+
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/will/criminalintent/fragment/AboutFragment.kt
+++ app/src/main/java/com/will/criminalintent/fragment/AboutFragment.kt
@@ -1,13 +1,22 @@
 package com.will.criminalintent.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.will.criminalintent.R
 
 class AboutFragment: Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 需要加这行代码
+        setHasOptionsMenu(true)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -15,5 +24,13 @@ class AboutFragment: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_about, container, false)
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+//        inflater.inflate(R.menu.fragment_crime_list, menu)
+        menu.clear()
+        // 可以清空 activity 的 menu
+        Log.e("WillWolf", "onCreateOptionsMenu-->")
     }
 }
